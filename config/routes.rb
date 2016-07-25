3ca403e971bf505c12258cacc7952f3a370cb20c
Anupama Dhir <sabhssabhs@yahoo.com>
2016-07-25 16:14:22 +0800

    changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  root 'application#welcome'#controller#method 
+  resources :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
