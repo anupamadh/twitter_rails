f1848d2596df21425111adf238719b18832abcc9
Anupama Dhir <sabhssabhs@yahoo.com>
2016-07-25 19:41:11 +0800

    made changes to the .erb files for about, help and home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def welcome
-    render html: "hello, rails!"
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'application#welcome' #controller#method
+  resources :posts
+  root 'static_pages#home' #controller#method
   resources :users
+
+  get '/home', to:'static_pages#home'
+  get '/about', to:'static_pages#about'
+  get '/help', to:'static_pages#help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
